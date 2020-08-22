7010bfc12b2c32facda7478b6c2ea64b639604b4
DmitryScaletta <dmitryscaletta@mail.ru>
2020-08-23 01:00:29 +0300

    feat: add cors and recent messages

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,25 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
+import recentMessages from 'recent-messages';
 
 dotenv.config();
 
 const app = express();
 
-app.get('/', async (req, res) => {
-  res.send('Hello world!');
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+
+app.get(
+  '/api/v1/recent-messages/:channel',
+  recentMessages({
+    channels: process.env.TWITCH_CHANNELS!.split(',').map((s) => s.trim()),
+    mongodbUri: process.env.MONGODB_URI!,
+    messagesLimit: Number.parseInt(process.env.MESSAGES_LIMIT!, 10),
+  }),
+);
+
+app.get('*', (req, res) => {
+  res.status(404);
 });
 
 app.listen(parseInt(process.env.PORT!));
