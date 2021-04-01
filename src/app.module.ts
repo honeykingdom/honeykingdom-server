4ccab7197de2b1429fa68fac07e350688f5eb7d9
DmitryScaletta <dmitryscaletta@mail.ru>
2021-04-01 12:25:35 +0300

    🩹 Add validationSchema and change config vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Config } from 'src/config/config.interface';
+import { Config, validationSchema } from 'src/config/config.interface';
 import { Message } from 'src/recent-messages/entities/message.entity';
 import { RecentMessagesModule } from 'src/recent-messages/recent-messages.module';
 import { HerokuAwakeModule } from './heroku-awake/heroku-awake.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ validationSchema }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService<Config>) => ({
         type: 'mongodb',
-        url: configService.get<string>('MONGODB_URI'),
+        url: configService.get<string>('RECENT_MESSAGES_MONGODB_URI'),
         entities: [Message],
-        synchronize: true,
+        synchronize: process.env.NODE_ENV === 'development',
       }),
       inject: [ConfigService],
     }),
--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -1,8 +1,30 @@
+import * as Joi from 'joi';
+
 export interface Config {
   PORT: string;
-  TWITCH_CHANNELS: string;
-  MESSAGES_LIMIT: string;
-  CORS_ORIGIN: string;
-  MONGODB_URI: string;
-  DEFAULT_RECENT_MESSAGES_URL: string;
+
+  RECENT_MESSAGES_CHANNELS: string;
+  RECENT_MESSAGES_LIMIT: string;
+  RECENT_MESSAGES_CORS_ORIGIN: string;
+  RECENT_MESSAGES_MONGODB_URI: string;
+  RECENT_MESSAGES_REDIRECT_URL: string;
+
+  HEROKU_AWAKE_BASE_URL: string;
+  HEROKU_AWAKE_INTERVAL: string;
 }
+
+export const validationSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production')
+    .default('development'),
+  PORT: Joi.number().default(3000),
+
+  RECENT_MESSAGES_CHANNELS: Joi.string().required(),
+  RECENT_MESSAGES_LIMIT: Joi.string().required(),
+  RECENT_MESSAGES_CORS_ORIGIN: Joi.string().required(),
+  RECENT_MESSAGES_MONGODB_URI: Joi.string().required(),
+  RECENT_MESSAGES_REDIRECT_URL: Joi.string().required(),
+
+  HEROKU_AWAKE_BASE_URL: Joi.string().required(),
+  HEROKU_AWAKE_INTERVAL: Joi.string().required(),
+});
