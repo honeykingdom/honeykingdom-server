4ac09449e5dba60963c70aae957f24b1311dfc39
DmitryScaletta <dmitryscaletta@mail.ru>
2021-04-11 22:48:48 +0300

    ✨ Add say moethod

--- src/twitch-chat/twitch-chat.service.ts
+++ src/twitch-chat/twitch-chat.service.ts
@@ -1,16 +1,20 @@
 import { Injectable, Scope } from '@nestjs/common';
-import { Chat, ChatEvents, PrivateMessage } from 'twitch-js';
-import { TwitchChatOptions } from './twitch-chat.types';
+import { Chat, ChatEvents, Commands, PrivateMessage } from 'twitch-js';
+import { TwitchChatModuleOptions } from './twitch-chat-options.interface';
 
 @Injectable({ scope: Scope.TRANSIENT })
 export class TwitchChatService {
   private readonly chat: Chat;
 
-  constructor(options: TwitchChatOptions) {
+  constructor(options: TwitchChatModuleOptions) {
     this.chat = new Chat({ log: { level: Infinity }, ...options });
 
     this.chat.on(ChatEvents.CONNECTED, () => console.log('connected'));
     this.chat.on(ChatEvents.DISCONNECTED, () => console.log('disconnected'));
+
+    this.chat.on(Commands.JOIN, ({ channel }) =>
+      console.log(`join: ${channel}`),
+    );
   }
 
   connect() {
@@ -25,11 +29,15 @@ export class TwitchChatService {
     return this.chat.part(channel);
   }
 
+  say(channel: string, message: string) {
+    this.chat.say(channel, message);
+  }
+
   addChatListener(listener: (message: PrivateMessage) => void) {
-    this.chat.on('PRIVMSG', listener);
+    this.chat.on(Commands.PRIVATE_MESSAGE, listener);
   }
 
   removeChatListener(listener: (message: PrivateMessage) => void) {
-    this.chat.off('PRIVMSG', listener);
+    this.chat.off(Commands.PRIVATE_MESSAGE, listener);
   }
 }
