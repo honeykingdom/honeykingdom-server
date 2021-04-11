fc88292608845312c8ac828c4ec170629e2e85e4
DmitryScaletta <dmitryscaletta@mail.ru>
2021-04-11 22:53:47 +0300

    ✨ Add honey-bot module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,22 +5,25 @@ import { Config, validationSchema } from 'src/config/config.interface';
 import { Message } from 'src/recent-messages/entities/message.entity';
 import { RecentMessagesModule } from 'src/recent-messages/recent-messages.module';
 import { HerokuAwakeModule } from './heroku-awake/heroku-awake.module';
+import { HoneyBotModule } from './honey-bot/honey-bot.module';
+import { TelegramChannel } from './telegram-api/entities/telegram-channel.entity';
 
 @Module({
   imports: [
+    HerokuAwakeModule,
     ConfigModule.forRoot({ validationSchema }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService<Config>) => ({
         type: 'mongodb',
         url: configService.get<string>('RECENT_MESSAGES_MONGODB_URI'),
-        entities: [Message],
+        entities: [Message, TelegramChannel],
         synchronize: process.env.NODE_ENV === 'development',
       }),
       inject: [ConfigService],
     }),
     RecentMessagesModule,
-    HerokuAwakeModule,
+    HoneyBotModule,
   ],
   providers: [],
   controllers: [],
