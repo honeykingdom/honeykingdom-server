5c9746a66d09b185d5bcea7f559dd532707dbaa5
DmitryScaletta <dmitryscaletta@mail.ru>
2021-04-11 22:52:15 +0300

    🩹 Add new config variables

--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -11,6 +11,13 @@ export interface Config {
 
   HEROKU_AWAKE_BASE_URL: string;
   HEROKU_AWAKE_INTERVAL: string;
+
+  TELEGRAM_API_CHECK_INTERVAL: string;
+
+  HONEY_BOT_USERNAME: string;
+  HONEY_BOT_TOKEN: string;
+  HONEY_BOT_CHANNELS: string;
+  HONEY_BOT_TELEGRAM_TO_CHAT: string;
 }
 
 export const validationSchema = Joi.object({
@@ -20,11 +27,18 @@ export const validationSchema = Joi.object({
   PORT: Joi.number().default(3000),
 
   RECENT_MESSAGES_CHANNELS: Joi.string().required(),
-  RECENT_MESSAGES_LIMIT: Joi.string().required(),
+  RECENT_MESSAGES_LIMIT: Joi.number().default(500),
   RECENT_MESSAGES_CORS_ORIGIN: Joi.string().required(),
   RECENT_MESSAGES_MONGODB_URI: Joi.string().required(),
   RECENT_MESSAGES_REDIRECT_URL: Joi.string().required(),
 
   HEROKU_AWAKE_BASE_URL: Joi.string().required(),
-  HEROKU_AWAKE_INTERVAL: Joi.string().required(),
+  HEROKU_AWAKE_INTERVAL: Joi.number().default(15 * 60 * 1000), // 15 min
+
+  TELEGRAM_API_CHECK_INTERVAL: Joi.number().default(2 * 60 * 1000), // 2 min
+
+  HONEY_BOT_USERNAME: Joi.string().required(),
+  HONEY_BOT_TOKEN: Joi.string().required(),
+  HONEY_BOT_CHANNELS: Joi.string().required(),
+  HONEY_BOT_TELEGRAM_TO_CHAT: Joi.string().required(),
 });
