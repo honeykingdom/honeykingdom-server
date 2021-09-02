c70124d54ca98550a462ac0f1d3710cf131c75cd
DmitryScaletta <dmitryscaletta@mail.ru>
2021-09-02 13:49:21 +0300

    ♻️ Use axios for ping request

--- src/heroku-awake/heroku-awake.module.ts
+++ src/heroku-awake/heroku-awake.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { HerokuAwakeController } from './heroku-awake.controller';
 import { HerokuAwakeService } from './heroku-awake.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, HttpModule],
   providers: [HerokuAwakeService],
   controllers: [HerokuAwakeController],
 })
--- src/heroku-awake/heroku-awake.service.ts
+++ src/heroku-awake/heroku-awake.service.ts
@@ -1,27 +1,27 @@
-import * as https from 'https';
-import * as http from 'http';
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
-import { Config } from 'src/config/config.interface';
+import { Config } from '../config/config.interface';
 
 @Injectable()
 export class HerokuAwakeService {
   private readonly interval: number;
+  private readonly url: string;
 
-  constructor(private readonly configService: ConfigService<Config>) {
+  constructor(
+    private readonly configService: ConfigService<Config>,
+    private readonly httpService: HttpService,
+  ) {
     this.interval = Number.parseInt(
-      this.configService.get<string>('HEROKU_AWAKE_INTERVAL'),
+      this.configService.get('HEROKU_AWAKE_INTERVAL', { infer: true }),
     );
-
-    const baseUrl = this.configService.get<string>('HEROKU_AWAKE_BASE_URL');
-    const url = `${baseUrl}/api/ping`;
+    const baseUrl = this.configService.get('HEROKU_AWAKE_BASE_URL', {
+      infer: true,
+    });
+    this.url = `${baseUrl}/api/ping`;
 
     setInterval(() => {
-      if (url.startsWith('https')) {
-        https.get(url);
-      } else {
-        http.get(url);
-      }
+      this.httpService.get(this.url);
     }, this.interval);
   }
 }
