f27dcd0e6be0869b63fb3f5975cbd92af689d4c7
DmitryScaletta <dmitryscaletta@mail.ru>
2021-09-05 19:06:30 +0300

    ✨ Refresh token if api returned 401

--- src/honey-votes/users/users.service.ts
+++ src/honey-votes/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AxiosResponse } from 'axios';
 import { Repository } from 'typeorm';
 import { differenceInMinutes } from 'date-fns';
 import ms from 'ms';
@@ -10,6 +11,13 @@ import { Config } from '../../config/config.interface';
 import { TwitchApiService } from '../../twitch-api/twitch-api.service';
 import { SubTier, TwitchUserType } from '../honey-votes.interface';
 import { User } from './entities/User.entity';
+import {
+  CheckUserSubscriptionResponse,
+  GetChannelEditorsResponse,
+  GetModeratorsResponse,
+  GetUserFollowsResponse,
+  RefreshTokenResponse,
+} from '../../twitch-api/twitch-api.interface';
 
 type CheckUserTypesInput = {
   [TwitchUserType.Editor]?: boolean;
@@ -78,17 +86,18 @@ export class UsersService {
     const dbUser = await this.findOne(user.id);
 
     if (dbUser && dbUser.accessToken) {
+      // TODO: don't await this operation?
       console.log('revokeToken', dbUser.accessToken);
 
-      const response = await this.twitchApiService.revokeToken(
-        dbUser.accessToken,
-        this.clientId,
-      );
+      const response = await this.twitchApiService.revokeToken({
+        token: dbUser.accessToken,
+        client_id: this.clientId,
+      });
 
       console.log('revokeToken', response);
     }
 
-    return this.userRepo.save({ ...user, validatedAt: new Date() });
+    return this.userRepo.save(user);
   }
 
   async checkUserTypes(
@@ -151,29 +160,64 @@ export class UsersService {
     channel: User,
     user: User,
   ): Promise<{ isSub: boolean; tier?: SubTier }> {
-    const response = await this.twitchApiService.checkUserSubscription(
-      { broadcaster_id: channel.id, user_id: user.id },
-      { clientId: this.clientId, accessToken: user.accessToken },
-    );
+    let response: AxiosResponse<CheckUserSubscriptionResponse>;
+    let accessToken = user.accessToken;
 
-    if (response.status !== 200) return { isSub: false };
+    while (true) {
+      try {
+        response = await this.twitchApiService.checkUserSubscription(
+          { broadcaster_id: channel.id, user_id: user.id },
+          { clientId: this.clientId, accessToken },
+        );
 
-    const isSub = response.data.data.length > 0;
+        break;
+      } catch (e) {
+        if (e.response.status === 401) {
+          const updatedUser = await this.refreshToken(user);
+
+          if (updatedUser === null) return { isSub: false };
+
+          accessToken = updatedUser.accessToken;
+        } else {
+          return { isSub: false };
+        }
+      }
+    }
+
     const tier = response.data.data[0].tier as SubTier;
 
-    return { isSub, tier };
+    return { isSub: true, tier };
   }
 
   async isFollower(
     channel: User,
     user: User,
   ): Promise<{ isFollower: boolean; minutesFollowed?: number }> {
-    const response = await this.twitchApiService.getUserFollows(
-      { from_id: user.id, to_id: channel.id },
-      { clientId: this.clientId, accessToken: user.accessToken },
-    );
+    let response: AxiosResponse<GetUserFollowsResponse>;
+    let accessToken = user.accessToken;
 
-    if (response.status !== 200) return { isFollower: false };
+    while (true) {
+      try {
+        response = await this.twitchApiService.getUserFollows(
+          { from_id: user.id, to_id: channel.id },
+          { clientId: this.clientId, accessToken },
+        );
+
+        break;
+      } catch (e) {
+        if (e.response.status === 401) {
+          const updatedUser = await this.refreshToken(user);
+
+          if (updatedUser === null) return { isFollower: false };
+
+          accessToken = updatedUser.accessToken;
+        } else {
+          return { isFollower: false };
+        }
+      }
+    }
+
+    if (response.data.total === 0) return { isFollower: false };
 
     const followedAt = new Date(response.data.data[0].followed_at);
     const minutesFollowed = differenceInMinutes(new Date(), followedAt);
@@ -182,28 +226,94 @@ export class UsersService {
   }
 
   async getChannelEditors(channel: User): Promise<Set<string>> {
-    const response = await this.twitchApiService.getChannelEditors(channel.id, {
-      clientId: this.clientId,
-      accessToken: channel.accessToken,
-    });
+    let response: AxiosResponse<GetChannelEditorsResponse>;
+    let accessToken = channel.accessToken;
 
-    if (response.status !== 200) return new Set();
+    while (true) {
+      try {
+        response = await this.twitchApiService.getChannelEditors(channel.id, {
+          clientId: this.clientId,
+          accessToken,
+        });
+
+        break;
+      } catch (e) {
+        if (e.response.status === 401) {
+          const updatedUser = await this.refreshToken(channel);
+
+          if (updatedUser === null) return new Set();
+
+          accessToken = updatedUser.accessToken;
+        } else {
+          return new Set();
+        }
+      }
+    }
 
     return new Set(response.data.data.map((u) => u.user_id));
   }
 
   private async getChannelMods(channel: User): Promise<Set<string>> {
-    const response = await this.twitchApiService.getModerators(channel.id, {
-      clientId: this.clientId,
-      accessToken: channel.accessToken,
-    });
+    let response: AxiosResponse<GetModeratorsResponse>;
+    let accessToken = channel.accessToken;
 
-    if (response.status !== 200) return new Set();
+    while (true) {
+      try {
+        response = await this.twitchApiService.getModerators(channel.id, {
+          clientId: this.clientId,
+          accessToken,
+        });
+
+        break;
+      } catch (e) {
+        if (e.response.status === 401) {
+          const updatedUser = await this.refreshToken(channel);
+
+          if (updatedUser === null) return new Set();
+
+          accessToken = updatedUser.accessToken;
+        } else {
+          return new Set();
+        }
+      }
+    }
 
     return new Set(response.data.data.map((u) => u.user_id));
   }
 
   private async getChannelVips(channelId: string): Promise<Set<string>> {
     return new Set();
+  }
+
+  /**
+   * Try to refresh tokens and write them to the db
+   * @returns null if refresh failed
+   * @returns the updated user if refresh succeeded
+   */
+  private async refreshToken(user: User): Promise<User | null> {
+    let response: AxiosResponse<RefreshTokenResponse>;
+
+    try {
+      response = await this.twitchApiService.refreshToken({} as any);
+    } catch (e) {
+      if (e.response.status === 400) {
+        await this.userRepo.save({ ...user, areTokensValid: false });
+
+        return null;
+      }
+    }
+
+    const {
+      data: { access_token, refresh_token },
+    } = response;
+
+    const newUser = await this.userRepo.save({
+      ...user,
+      accessToken: access_token,
+      refreshToken: refresh_token,
+      areTokensValid: true,
+    });
+
+    return newUser;
   }
 }
