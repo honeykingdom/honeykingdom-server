42d5f61ef25e68ed1ba21d06ccde0a91e64a3878
DmitryScaletta <dmitryscaletta@mail.ru>
2021-09-06 15:03:32 +0300

    🤡 Add igdb api response

---
+++ test/igdb-api.mock.ts
@@ -0,0 +1,25 @@
+export const game379 = {
+  id: 379,
+  cover: {
+    id: 105899,
+    image_id: 'co29pn',
+  },
+  first_release_date: 1100649600,
+  genres: [
+    {
+      id: 5,
+      name: 'Shooter',
+    },
+    {
+      id: 24,
+      name: 'Tactical',
+    },
+    {
+      id: 31,
+      name: 'Adventure',
+    },
+  ],
+  name: 'Metal Gear Solid 3: Snake Eater',
+  release_dates: [28771, 28772, 28773, 28774],
+  slug: 'metal-gear-solid-3-snake-eater',
+};
