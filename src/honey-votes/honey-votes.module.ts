695447eb57ccc9fc2f9efc4e6660b4b41e0f8d6d
DmitryScaletta <dmitryscaletta@mail.ru>
2021-09-10 20:33:39 +0300

    🩹 Add users module

--- src/honey-votes/honey-votes.module.ts
+++ src/honey-votes/honey-votes.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ChatVotesModule } from './chat-votes/chat-votes.module';
+import { UsersModule } from './users/users.module';
 import { VotesModule } from './votes/votes.module';
 
 @Module({
-  imports: [AuthModule, VotesModule, ChatVotesModule],
+  imports: [AuthModule, UsersModule, VotesModule, ChatVotesModule],
 })
 export class HoneyVotesModule {}
