8fcaf3e02fce303157e8d8b4627604cf15a57c4e
DmitryScaletta <dmitryscaletta@mail.ru>
2021-09-10 13:47:30 +0300

    ✨ Add chat constants

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -1,1 +1,3 @@
-export const POSTGRES_CONNECTION = 'POSTGRES_CONNECTION';
+export const POSTGRES_CONNECTION = 'Postgres';
+export const TWITCH_CHAT_ANONYMOUS = 'Anonymous';
+export const TWITCH_CHAT_HONEYKINGDOM = 'HoneyKingdom';
