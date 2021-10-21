30acbaa61548079001db808214e4388d880dc944
DmitryScaletta <dmitryscaletta@mail.ru>
2021-10-21 14:37:14 +0300

    🚑 Add connection name

--- src/telegram-api/telegram-api.module.ts
+++ src/telegram-api/telegram-api.module.ts
@@ -4,12 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TelegramChannel } from './entities/telegram-channel.entity';
 import { TelegramApiService } from './telegram-api.service';
+import { POSTGRES_CONNECTION } from '../app.constants';
 
 @Module({
   imports: [
     ConfigModule,
     HttpModule,
-    TypeOrmModule.forFeature([TelegramChannel]),
+    TypeOrmModule.forFeature([TelegramChannel], POSTGRES_CONNECTION),
   ],
   providers: [TelegramApiService],
   controllers: [],
