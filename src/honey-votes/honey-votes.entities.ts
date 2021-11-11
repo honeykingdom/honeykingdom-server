6c01974e1faf95a506c9440a5ca9c7dfc21c27b5
DmitryScaletta <dmitryscaletta@mail.ru>
2021-11-11 18:52:07 +0300

    ✨ Register chat-goal module

--- src/honey-votes/honey-votes.entities.ts
+++ src/honey-votes/honey-votes.entities.ts
@@ -5,6 +5,9 @@ import { ChatVote } from './chat-votes/entities/ChatVote.entity';
 import { Voting } from './votes/entities/Voting.entity';
 import { VotingOption } from './votes/entities/VotingOption.entity';
 import { Vote } from './votes/entities/Vote.entity';
+import { ChatGoal } from './chat-goal/entities/chat-goal.entity';
+import { ChatGoalData } from './chat-goal/entities/chat-goal-data.entity';
+import { ChatGoalEvent } from './chat-goal/entities/chat-goal-event.entity';
 
 export const honeyVotesEntities = [
   User,
@@ -14,4 +17,7 @@ export const honeyVotesEntities = [
   Voting,
   VotingOption,
   Vote,
+  ChatGoal,
+  ChatGoalEvent,
+  ChatGoalData,
 ];
--- src/honey-votes/honey-votes.module.ts
+++ src/honey-votes/honey-votes.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ChatGoalModule } from './chat-goal/chat-goal.module';
 import { ChatVotesModule } from './chat-votes/chat-votes.module';
 import { UsersModule } from './users/users.module';
 import { VotesModule } from './votes/votes.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, VotesModule, ChatVotesModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    VotesModule,
+    ChatVotesModule,
+    ChatGoalModule,
+  ],
 })
 export class HoneyVotesModule {}
