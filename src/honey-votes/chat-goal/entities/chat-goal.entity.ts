5079c56f2a98327184b7965f3641cfb41f1f1feb
DmitryScaletta <dmitryscaletta@mail.ru>
2021-11-12 11:08:30 +0300

    🩹 Add default values for swagger

--- src/honey-votes/chat-goal/entities/chat-goal.entity.ts
+++ src/honey-votes/chat-goal/entities/chat-goal.entity.ts
@@ -51,53 +51,68 @@ export class ChatGoal {
   data: ChatGoalData;
 
   @Column({ type: 'jsonb', default: CHAT_GOAL_PERMISSIONS_DEFAULT })
-  @ApiProperty()
+  @ApiProperty({ default: CHAT_GOAL_PERMISSIONS_DEFAULT })
   permissions: ChatGoalPermissions;
 
   @Column({ default: false })
-  @ApiProperty()
+  @ApiProperty({ default: false })
   listening: boolean;
 
   @Column({ default: '', length: CHAT_GOAL_TITLE_MAX_LENGTH })
-  @ApiProperty()
+  @ApiProperty({ default: '', maxLength: CHAT_GOAL_TITLE_MAX_LENGTH })
   title: string;
 
   @Column({
     default: CHAT_GOAL_UPVOTE_COMMAND_DEFAULT,
     length: CHAT_GOAL_VOTE_COMMAND_MAX_LENGTH,
   })
-  @ApiProperty()
+  @ApiProperty({
+    default: CHAT_GOAL_UPVOTE_COMMAND_DEFAULT,
+    maxLength: CHAT_GOAL_VOTE_COMMAND_MAX_LENGTH,
+  })
   upvoteCommand: string;
 
   @Column({
     default: CHAT_GOAL_DOWNVOTE_COMMAND_DEFAULT,
     length: CHAT_GOAL_VOTE_COMMAND_MAX_LENGTH,
   })
-  @ApiProperty()
+  @ApiProperty({
+    default: CHAT_GOAL_DOWNVOTE_COMMAND_DEFAULT,
+    maxLength: CHAT_GOAL_VOTE_COMMAND_MAX_LENGTH,
+  })
   downvoteCommand: string;
 
   @Column({ default: 0 })
-  @ApiProperty({ description: 'Original timer duration' })
+  @ApiProperty({ description: 'Original timer duration', default: 0 })
   timerDuration: number;
 
   @Column({ default: CHAT_VOTES_MAX_VOTES_VALUE_DEFAULT })
-  @ApiProperty({ description: 'How many votes needs to complete the goal' })
+  @ApiProperty({
+    description: 'How many votes needs to complete the goal',
+    default: CHAT_VOTES_MAX_VOTES_VALUE_DEFAULT,
+  })
   maxVotesValue: number;
 
   @Column({ default: ChatGoalStatus.Uninitialized })
-  @ApiProperty()
+  @ApiProperty({ default: ChatGoalStatus.Uninitialized })
   status: ChatGoalStatus;
 
   @Column({ default: 0 })
-  @ApiProperty({ description: 'Timestamp when timer should finish' })
+  @ApiProperty({
+    description: 'Timestamp when timer should finish',
+    default: 0,
+  })
   endTimerTimestamp: number;
 
   @Column({ default: 0 })
-  @ApiProperty({ description: 'Remaining timer duration after pause' })
+  @ApiProperty({
+    description: 'Remaining timer duration after pause',
+    default: 0,
+  })
   remainingTimerDuration: number;
 
   @Column({ default: 0 })
-  @ApiProperty({ description: 'Current full votes value' })
+  @ApiProperty({ description: 'Current full votes value', default: 0 })
   votesValue: number;
 
   @CreateDateColumn({ type: 'timestamptz' })
