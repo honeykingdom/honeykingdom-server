20d8eb2c6e7fc89f6db1ce1f95be535241d324e5
DmitryScaletta <dmitryscaletta@mail.ru>
2022-01-05 19:16:37 +0300

    🩹 Update getLastPostId method

--- src/instagram/instagram.service.ts
+++ src/instagram/instagram.service.ts
@@ -7,27 +7,18 @@ export class InstagramService {
   constructor(private readonly httpService: HttpService) {}
 
   async getLastPostId(nickname: string): Promise<string> {
-    const response = await lastValueFrom(
-      this.httpService.get<string>(`https://www.instagram.com/${nickname}/`),
-    );
+    let id = '';
 
-    const startText = '<script type="text/javascript">window._sharedData = ';
-    const endText =
-      ';</script>\n<script type="text/javascript">window.__initialDataLoaded';
+    try {
+      const response = await lastValueFrom(
+        this.httpService.get(`https://www.instagram.com/${nickname}/?__a=1`),
+      );
 
-    const startIndex = response.data.indexOf(startText);
-    const endIndex = response.data.indexOf(endText);
+      id =
+        response.data.graphql.user.edge_owner_to_timeline_media.edges[0].node
+          .shortcode;
+    } catch (e) {}
 
-    const jsonText = response.data.substring(
-      startIndex + startText.length,
-      endIndex,
-    );
-    const json = JSON.parse(jsonText);
-
-    const shortcode =
-      json.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media
-        .edges[0].node.shortcode;
-
-    return shortcode;
+    return id;
   }
 }
