7e3a91b99721cc5f944a3eb0c29751ecfead55f5
DmitryScaletta <dmitryscaletta@mail.ru>
2022-01-16 16:46:38 +0300

    ✨ Add cookie to the request

--- src/instagram/instagram.service.ts
+++ src/instagram/instagram.service.ts
@@ -7,11 +7,40 @@ import NodeCache from 'node-cache';
 export class InstagramService {
   private readonly logger = new Logger(InstagramService.name);
 
-  private readonly lastPostUrlsCache = new NodeCache({ stdTTL: 60 * 60 }); // 1 hour
+  private readonly lastPostUrlsCache = new NodeCache({ stdTTL: 60 * 30 }); // 30 min
 
   constructor(private readonly httpService: HttpService) {}
 
   async getLastPostUrl(nickname: string): Promise<string> {
+    if (this.lastPostUrlsCache.has(nickname)) {
+      return this.lastPostUrlsCache.get(nickname);
+    }
+
+    try {
+      const pageUrl = `https://www.instagram.com/${nickname}/?__a=1`;
+      const cookie = `sessionid=${process.env.INSTAGRAM_SESSION_ID}`;
+      const response = await lastValueFrom(
+        this.httpService.get(pageUrl, { headers: { cookie } }),
+      );
+
+      const lastPostId =
+        response.data.graphql.user.edge_owner_to_timeline_media.edges[0].node
+          .shortcode;
+      const url = `https://www.instagram.com/p/${lastPostId}/`;
+
+      this.logger.log(`user: ${nickname}, url: ${url}`);
+      this.lastPostUrlsCache.set(nickname, url);
+
+      return url;
+    } catch (e) {
+      console.error(e);
+    }
+
+    return `https://www.instagram.com/${nickname}/`;
+  }
+
+  /** @deprecated */
+  async getLastPostUrl_(nickname: string): Promise<string> {
     if (this.lastPostUrlsCache.has(nickname)) {
       return this.lastPostUrlsCache.get(nickname);
     }
