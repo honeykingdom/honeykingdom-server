17ed4c0b3278cbeb3a0b2665a1eab7a8b62e222b
DmitryScaletta <dmitryscaletta@mail.ru>
2022-01-17 22:35:13 +0300

    ⚰️ Remoce deprecated code

--- src/instagram/instagram.service.ts
+++ src/instagram/instagram.service.ts
@@ -38,43 +38,4 @@ export class InstagramService {
 
     return `https://www.instagram.com/${nickname}/`;
   }
-
-  /** @deprecated */
-  async getLastPostUrl_(nickname: string): Promise<string> {
-    if (this.lastPostUrlsCache.has(nickname)) {
-      return this.lastPostUrlsCache.get(nickname);
-    }
-
-    try {
-      const pageUrl = `https://www.anonigviewer.com/profile.php?u=${nickname}&c=posts`;
-      const page = await lastValueFrom(this.httpService.get<string>(pageUrl));
-
-      const sr = page.data.match(/var sr = "([^;]+)";/)[1];
-
-      const response = await lastValueFrom(
-        this.httpService.post(
-          'https://www.anonigviewer.com/fetch.php',
-          `sr=${sr}&a=`,
-          {
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              Referer: pageUrl,
-            },
-          },
-        ),
-      );
-
-      const lastPostId = response.data.d.posts[0].shortcode;
-      const url = `https://www.instagram.com/p/${lastPostId}/`;
-
-      this.logger.log(`user: ${nickname}, url: ${url}`);
-      this.lastPostUrlsCache.set(nickname, url);
-
-      return url;
-    } catch (e) {
-      console.error(e);
-    }
-
-    return `https://www.instagram.com/${nickname}/`;
-  }
 }
