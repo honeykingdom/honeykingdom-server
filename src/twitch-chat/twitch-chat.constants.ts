1317a661c0f5baa50d07ee710e4fbcd4a6677fea
DmitryScaletta <dmitryscaletta@mail.ru>
2022-01-21 05:17:29 +0300

    ✨ Add twitch chat constants

--- src/twitch-chat/twitch-chat.constants.ts
+++ src/twitch-chat/twitch-chat.constants.ts
@@ -1,2 +1,57 @@
 export const TWITCH_CHAT_CONNECTION = 'TwitchChatConnection';
 export const TWITCH_CHAT_MODULE = 'TwitchChatModule';
+
+/**
+ * Tier 1: 0-96
+ * Tier 2: 2000-2096
+ * Tier 3: 3000-3096
+ */
+export const SubBadge = {
+  0: 'Subscriber',
+  3: '3-Month Subscriber',
+  6: '6-Month Subscriber',
+  12: '1-Year Subscriber',
+  24: '2-Year Subscriber',
+  36: '3-Year Subscriber',
+  48: '4-Year Subscriber',
+  54: '4.5-Year Subscriber',
+  60: '5-Year Subscriber',
+  66: '5.5-Year Subscriber',
+  72: '6-Year Subscriber',
+  78: '6.5-Year Subscriber',
+  84: '7-Year Subscriber',
+  90: '7.5-Year Subscriber',
+  96: '8-Year Subscriber',
+
+  2000: 'Subscriber',
+  2003: '3-Month Subscriber',
+  2006: '6-Month Subscriber',
+  2012: '1-Year Subscriber',
+  2024: '2-Year Subscriber',
+  2036: '3-Year Subscriber',
+  2048: '4-Year Subscriber',
+  2054: '4.5-Year Subscriber',
+  2060: '5-Year Subscriber',
+  2066: '5.5-Year Subscriber',
+  2072: '6-Year Subscriber',
+  2078: '6.5-Year Subscriber',
+  2084: '7-Year Subscriber',
+  2090: '7.5-Year Subscriber',
+  2096: '8-Year Subscriber',
+
+  3000: 'Subscriber',
+  3003: '3-Month Subscriber',
+  3006: '6-Month Subscriber',
+  3012: '1-Year Subscriber',
+  3024: '2-Year Subscriber',
+  3036: '3-Year Subscriber',
+  3048: '4-Year Subscriber',
+  3054: '4.5-Year Subscriber',
+  3060: '5-Year Subscriber',
+  3066: '5.5-Year Subscriber',
+  3072: '6-Year Subscriber',
+  3078: '6.5-Year Subscriber',
+  3084: '7-Year Subscriber',
+  3090: '7.5-Year Subscriber',
+  3096: '8-Year Subscriber',
+};
