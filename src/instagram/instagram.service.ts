341206980ef0a0d1c731b8ac5e7fad46e03d6d7e
DmitryScaletta <dmitryscaletta@mail.ru>
2022-06-30 22:58:11 +0300

    🚑 Fix getting instagram user info

--- src/instagram/instagram.service.ts
+++ src/instagram/instagram.service.ts
@@ -7,6 +7,8 @@ import { Cache } from 'cache-manager';
 export class InstagramService {
   static CACHE_TTL = 60 * 30; // 30 min
 
+  private static readonly INSTAGRAM_APP_ID = '936619743392459';
+
   private readonly logger = new Logger(InstagramService.name);
 
   constructor(
@@ -22,8 +24,7 @@ export class InstagramService {
     try {
       const userInfo = await this.getUserInfo(nickname);
       const lastPostId =
-        userInfo.graphql.user.edge_owner_to_timeline_media.edges[0].node
-          .shortcode;
+        userInfo.data.user.edge_owner_to_timeline_media.edges[0].node.shortcode;
       const url = `https://www.instagram.com/p/${lastPostId}/`;
 
       this.logger.log(`user: ${nickname}, url: ${url}`);
@@ -38,10 +39,11 @@ export class InstagramService {
   }
 
   private async getUserInfo(nickname: string): Promise<any> {
-    const url = `https://www.instagram.com/${nickname}/?__a=1`;
-    const cookie = `sessionid=${process.env.INSTAGRAM_SESSION_ID}`;
+    const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${nickname}`;
     const response = await lastValueFrom(
-      this.httpService.get(url, { headers: { cookie } }),
+      this.httpService.get(url, {
+        headers: { 'x-ig-app-id': InstagramService.INSTAGRAM_APP_ID },
+      }),
     );
 
     return response.data;
