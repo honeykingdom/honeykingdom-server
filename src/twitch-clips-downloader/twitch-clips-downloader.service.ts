6646a14268d888d5c53418dd38926943c4682edf
DmitryScaletta <dmitryscaletta@mail.ru>
2023-10-23 11:58:26 +0300

    ♻️ Refactor http requests

--- src/twitch-clips-downloader/twitch-clips-downloader.service.ts
+++ src/twitch-clips-downloader/twitch-clips-downloader.service.ts
@@ -1,5 +1,8 @@
-import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger, OnModuleDestroy } from '@nestjs/common';
 import { escapers } from '@telegraf/entity';
+import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { lastValueFrom } from 'rxjs';
 import {
   SIZE_20_MB,
   THUMBNAIL_REGEX,
@@ -16,57 +19,64 @@ import {
 export class TwitchClipsDownloaderService implements OnModuleDestroy {
   private accessToken = '';
 
+  private readonly logger = new Logger(TwitchClipsDownloaderService.name);
+
+  constructor(private readonly httpService: HttpService) {}
+
   async onModuleDestroy() {
     if (this.accessToken) this.revokeAccessToken();
   }
 
+  private http<T = any>(
+    config: AxiosRequestConfig<any>,
+  ): Promise<AxiosResponse<T>> {
+    return lastValueFrom(this.httpService.request(config));
+  }
+
   // https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#client-credentials-grant-flow
   private async getAccessToken() {
-    const res = await fetch('https://id.twitch.tv/oauth2/token', {
+    const res = await this.http<TwitchTokenResponse>({
+      url: 'https://id.twitch.tv/oauth2/token',
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
+      data: new URLSearchParams({
         client_id: process.env.TG_BOT_TWITCH_CLIENT_ID,
         client_secret: process.env.TG_BOT_TWITCH_CLIENT_SECRET,
         grant_type: 'client_credentials',
-      }).toString(),
+      }),
     });
-    if (res.status !== 200) {
+    if (!res.data?.access_token) {
       throw new Error('Cannot get twitch access token');
     }
-    const json = (await res.json()) as TwitchTokenResponse;
-    if (!json?.access_token) {
-      throw new Error('Cannot get twitch access token');
-    }
-    return json;
+    return res.data;
   }
 
   private async fetchAndStoreToken() {
-    const auth = await this.getAccessToken();
-    this.accessToken = auth.access_token;
-    console.log(`New access token: ${this.accessToken}`);
+    try {
+      const auth = await this.getAccessToken();
+      this.accessToken = auth.access_token;
+      this.logger.log(`New access token: ${this.accessToken}`);
+    } catch (e) {
+      this.logger.error(e.message, e.stack);
+    }
   }
 
   // https://dev.twitch.tv/docs/authentication/revoke-tokens/
   private async revokeAccessToken() {
-    const res = await fetch(`https://id.twitch.tv/oauth2/revoke`, {
+    const res = await this.http({
+      url: 'https://id.twitch.tv/oauth2/revoke',
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
+      data: new URLSearchParams({
         client_id: process.env.TG_BOT_TWITCH_CLIENT_ID,
         token: this.accessToken,
-      }).toString(),
+      }),
     });
-    return res.json();
+    return res.data;
   }
 
   // https://dev.twitch.tv/docs/api/reference#get-clips
-  private async fetchClip(slug: string) {
-    return fetch(`https://api.twitch.tv/helix/clips?id=${slug}`, {
+  private fetchClip(slug: string) {
+    return this.http<GetClipsResponse>({
+      url: `https://api.twitch.tv/helix/clips?id=${slug}`,
       headers: {
         Authorization: `Bearer ${this.accessToken}`,
         'Client-ID': process.env.TG_BOT_TWITCH_CLIENT_ID,
@@ -76,49 +86,63 @@ export class TwitchClipsDownloaderService implements OnModuleDestroy {
 
   private async getClip(slug: string) {
     if (!this.accessToken) await this.fetchAndStoreToken();
-    let res = await this.fetchClip(slug);
-    if (res.status === 401) {
-      await this.fetchAndStoreToken();
+    let res: AxiosResponse<GetClipsResponse>;
+    try {
       res = await this.fetchClip(slug);
+    } catch (e) {
+      if (e.response?.status === 401) {
+        await this.fetchAndStoreToken();
+        res = await this.fetchClip(slug);
+      }
     }
-    if (res.status !== 200) {
-      throw new Error(`Cannot find the clip: ${slug}`);
+    const clip = res.data.data[0];
+    if (!clip) throw new Error(`Cannot find the clip: ${slug}`);
+    return clip;
+  }
+
+  private async getUrlContentLength(url: string): Promise<number | null> {
+    let res: AxiosResponse<any, any>;
+    try {
+      res = await this.http({ url, method: 'HEAD' });
+    } catch (e) {
+      return null;
     }
-    const json = (await res.json()) as GetClipsResponse;
-    return json.data[0];
+    const contentLengthText = res.headers['content-length'];
+    if (!contentLengthText) return null;
+    return Number.parseInt(contentLengthText);
   }
 
   async getClipInfo(url: string): Promise<ClipInfo | null> {
     const m = TWITCH_CLIP_REGEX.exec(url);
     if (!m) return null;
+    this.logger.log(url);
     const slug = m[1];
     let clip: Clip | undefined;
     try {
       clip = await this.getClip(slug);
     } catch (e) {
-      console.error(e);
+      this.logger.error(e.message, e.stack);
       return null;
     }
     if (!THUMBNAIL_REGEX.test(clip?.thumbnail_url || '')) return null;
     const downloadLink = clip.thumbnail_url.replace(THUMBNAIL_REGEX, '.mp4');
-    const res = await fetch(downloadLink, { method: 'HEAD' });
-    const sizeText = res.headers.get('Content-Length');
-    if (!sizeText) return null;
     const title = escapers.MarkdownV2(clip?.title);
     const channel = escapers.MarkdownV2(clip?.broadcaster_name);
     const author = escapers.MarkdownV2(clip?.creator_name);
     const caption = `${title}\n\n*Channel*: _${channel}_ \\| *Created by*: _${author}_ \\| *Views*: _${clip?.view_count}_`;
-    const size = Number.parseInt(sizeText);
-    return size > SIZE_20_MB
-      ? {
-          type: 'photo',
-          url: clip.thumbnail_url,
-          caption: `${caption}\n\n⚠️ _Can't upload more than 20MB, please use [this link](${downloadLink}) to download\\._`,
-        }
-      : {
-          type: 'video',
-          url: downloadLink,
-          caption: caption,
-        };
+    const size = await this.getUrlContentLength(downloadLink);
+    if (!size) return null;
+    if (size > SIZE_20_MB) {
+      return {
+        type: 'photo',
+        url: clip.thumbnail_url,
+        caption: `${caption}\n\n⚠️ _Can't upload more than 20MB, please use [this link](${downloadLink}) to download\\._`,
+      };
+    }
+    return {
+      type: 'video',
+      url: downloadLink,
+      caption: caption,
+    };
   }
 }
